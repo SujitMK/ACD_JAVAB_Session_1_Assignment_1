858eaf94ecd02dd18f216a5089d5964130f2c585
Sujit <sujitmk007@gmail.com>
2017-09-21 21:44:39 +0530

    Java - Session 1 Assignment 1

---
+++ Sujit_ACD/src/ACD_JAVAB_Session_1_Assignment_1/HelloWorld.java
@@ -0,0 +1,12 @@
+package ACD_JAVAB_Session_1_Assignment_1;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		
+		//Prints the Hello World statement		
+	     System.out.println("Hello World! My name is Sujit.");
+
+	}
+
+}
